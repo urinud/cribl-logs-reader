54b039403fe1ee1b0f8ee4b19f35c08ec026ae10
Uriel Nudelman <urinud@gmail.com>
2024-12-23 23:20:10 -0500

    Front End Init
    Signed-off-by: Uriel Nudelman <urinud@gmail.com>

--- index.js
+++ index.js
@@ -1,47 +1,66 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
-const LOG_DIR = '/var/log';
+const LOG_DIR = "/var/log";
+
+// Configure Static Content
+app.use(express.static("public"));
 
 // Returns the list of log files in the log directory
-app.get('/logs', (req, res) => {
-    fs.readdir(LOG_DIR, (err, files) => {
-        if (err) {
-            console.error('Error reading log directory: ' + err);
-            return res.status(500).send('Unable to read log directory: ' + err);
-        } else {
-            const logFiles = files.filter(file => fs.statSync(path.join(LOG_DIR, file)).isFile());
-            res.json({ files : logFiles });
-        }
-    });
+app.get("/logs", (req, res) => {
+  fs.readdir(LOG_DIR, (err, files) => {
+    if (err) {
+      console.error("Error reading log directory: " + err);
+      return res.status(500).send("Unable to read log directory: " + err);
+    } else {
+      const logFiles = files.filter((file) =>
+        fs.statSync(path.join(LOG_DIR, file)).isFile()
+      );
+      res.json({ files: logFiles });
+    }
+  });
 });
 
 // Returns the content of the log file
-app.get('/logs/:filename', (req, res) => {
-   const { filename } = req.params;
-   const { regex, lines } = req.query;
-   const filePath = path.join(LOG_DIR, filename);
+app.get("/logs/:filename", (req, res) => {
+  const { filename } = req.params;
+  const { regex, lines } = req.query;
+  const filePath = path.join(LOG_DIR, filename);
 
-   if (!fs.existsSync(filePath)) {
-       return res.status(404).send('File not found');
-   }
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send("File not found");
+  }
 
-   try {
-       const lines = [];
-       res.json({ lines : lines });
-   } catch (err) {
-       console.error('Error reading log file: ' + err);
-       res.status(500).send('Unable to read log file ' + filename + ' with error: ' + err);
-   }
+  const lineCount = lines ? parseInt(lines) : 25;
+  if (isNaN(lineCount) || lineCount <= 0) {
+    return res
+      .status(400)
+      .send(
+        "Invalid lines count. Check lines parameter, must be positive integer"
+      );
+  }
+
+  try {
+    const lines = [];
+    lines.push('Filename: ' + filename);
+    lines.push('Regex: ' + regex);
+    lines.push('Lines: ' + lineCount);
+    res.json({ lines: lines });
+  } catch (err) {
+    console.error("Error reading log file: " + err);
+    res
+      .status(500)
+      .send("Unable to read log file " + filename + " with error: " + err);
+  }
 });
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port " + PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
